29c1abc89586a2027ddd61b32483a90539d81db0
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2017-11-14 23:57:18 +0000

    More work

---
+++ ModernUIDoneRight/Forms/MetroForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NickAc.ModernUIDoneRight.Forms
+{
+    public class MetroForm : Form
+    {
+        #region Constructor
+        public MetroForm()
+        {
+            BackColor = Color.White;
+            FormBorderStyle = FormBorderStyle.None;
+        }
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+
+        #region Methods
+
+        #endregion
+
+        #region Overriden Methods
+
+        #endregion
+
+    }
+}
