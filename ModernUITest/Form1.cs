9e00206a5d39c28b1feb0aaa10dadd355bf916a6
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2017-11-15 14:52:38 +0000

    Add test project. Move to .NET Framework 3.5 Client Profile

---
+++ ModernUITest/Form1.cs
@@ -0,0 +1,20 @@
+﻿using NickAc.ModernUIDoneRight.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ModernUITest
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
