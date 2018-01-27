015f3e20f00f2766d13da72f413f4d270531e464
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2018-01-27 23:07:27 +0000

    Allow to freeze the shadow rendering on the shadow panel

--- ModernUIDoneRight/Controls/ModernShadowPanel.cs
+++ ModernUIDoneRight/Controls/ModernShadowPanel.cs
@@ -1,18 +1,59 @@
 ﻿using NickAc.ModernUIDoneRight.Utils;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace NickAc.ModernUIDoneRight.Controls
 {
     public class ModernShadowPanel : Panel
     {
+        private Bitmap FrozenImage { get; set; }
+
+        public void Freeze()
+        {
+            FrozenImage = new Bitmap(Size.Width, Size.Height);
+            using (var g = Graphics.FromImage(FrozenImage)) {
+                foreach (Control c in Controls) {
+                    DrawControlShadow(c, g);
+                }
+            }
+            Refresh();
+        }
+
+        public void Unfreeze()
+        {
+            FrozenImage = null;
+            Refresh();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            if (FrozenImage != null)
+                Freeze();
+        }
+
+        protected override void OnControlAdded(ControlEventArgs e)
+        {
+            base.OnControlAdded(e);
+            if (FrozenImage != null)
+                Freeze();
+        }
+
+        protected override void OnControlRemoved(ControlEventArgs e)
+        {
+            base.OnControlRemoved(e);
+            if (FrozenImage != null)
+                Freeze();
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
+            if (FrozenImage != null) {
+                e.Graphics.DrawImage(FrozenImage, Point.Empty);
+                return;
+            }
             foreach (Control c in Controls) {
                 DrawControlShadow(c, e.Graphics);
             }
